6a9322e3e76a2f990f68accf2efbcdc2272b1345
Ryo Takahashi <at.rtsan@gmail.com>
2012-03-10 20:20:49 -0800

    modify

---
+++ main.js
@@ -0,0 +1,13 @@
+enchant();
+var game;
+var player;
+
+window.onload = function() {
+    game = new Game(320, 320);
+    //game.preload();
+    game.onload = function() {
+
+
+    };
+    game.start();
+};
